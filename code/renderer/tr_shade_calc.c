e67ae3f79d5791252947db343c831dca1bf80972
eugene <eugene@832414e2-6573-9c40-a513-caa1f8131539>
2012-04-19 09:14:22 +0000

    Fix MSVC compiler warnings

--- code/renderer/tr_shade_calc.c
+++ code/renderer/tr_shade_calc.c
@@ -420,7 +420,7 @@ Autosprite2Deform
 Autosprite2 will pivot a rectangular quad along the center of its long axis
 =====================
 */
-int edgeVerts[6][2] = {
+unsigned int edgeVerts[6][2] = {
 	{ 0, 1 },
 	{ 0, 2 },
 	{ 0, 3 },
