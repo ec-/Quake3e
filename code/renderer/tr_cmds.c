d6d7c910c7d4f101723aca6aa185b08e02e7a0a6
eugene <eugene@832414e2-6573-9c40-a513-caa1f8131539>
2013-05-15 18:57:18 +0000

    Delete renderCommandList and renderThreadActive variables from renderer

--- code/renderer/tr_cmds.c
+++ code/renderer/tr_cmds.c
@@ -19,12 +19,8 @@ along with Quake III Arena source code; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ===========================================================================
 */
+
 #include "tr_local.h"
-
-volatile renderCommandList_t	*renderCommandList;
-
-volatile qboolean	renderThreadActive;
-
 
 /*
 =====================
