bcafd15c73c88720a6c2816b22ad1faadf19018a
eugene <eugene@832414e2-6573-9c40-a513-caa1f8131539>
2013-11-10 17:50:05 +0000

    ioq3: Clear more variables in CL_InitServerInfo

--- code/client/cl_main.c
+++ code/client/cl_main.c
@@ -1905,6 +1905,7 @@ void CL_InitServerInfo( serverInfo_t *server, netadr_t *address ) {
 	server->game[0] = '\0';
 	server->gameType = 0;
 	server->netType = 0;
+	server->punkbuster = 0;
 }
 
 #define MAX_SERVERSPERPACKET	256
@@ -3212,18 +3213,7 @@ void CL_ServerInfoPacket( netadr_t from, msg_t *msg ) {
 
 	// add this to the list
 	cls.numlocalservers = i+1;
-	cls.localServers[i].adr = from;
-	cls.localServers[i].clients = 0;
-	cls.localServers[i].hostName[0] = '\0';
-	cls.localServers[i].mapName[0] = '\0';
-	cls.localServers[i].maxClients = 0;
-	cls.localServers[i].maxPing = 0;
-	cls.localServers[i].minPing = 0;
-	cls.localServers[i].ping = -1;
-	cls.localServers[i].game[0] = '\0';
-	cls.localServers[i].gameType = 0;
-	cls.localServers[i].netType = from.type;
-	cls.localServers[i].punkbuster = 0;
+	CL_InitServerInfo( &cls.localServers[i], &from );
 									 
 	Q_strncpyz( info, MSG_ReadString( msg ), MAX_INFO_STRING );
 	if (strlen(info)) {
