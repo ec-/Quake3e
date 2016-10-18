ab70932b95ca579f1830ac060f9d7094776ef682
eugene <eugene@832414e2-6573-9c40-a513-caa1f8131539>
2016-10-18 11:56:27 +0000

    Use 11 bits for refentities (12 in opengl1 renderer)

--- code/renderer/tr_types.h
+++ code/renderer/tr_types.h
@@ -26,7 +26,12 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #define	MAX_DLIGHTS		32		// can't be increased, because bit flags are used on surfaces
 
-#define	REFENTITYNUM_BITS	10		// can't be increased without changing drawsurf bit packing
+#ifdef USE_RENDERER2
+#define	REFENTITYNUM_BITS	11	// can't be increased without changing drawsurf bit packing
+#else
+#define	REFENTITYNUM_BITS	12	// as we actually using only 1 bit for dlight mask in opengl1 renderer
+#endif
+
 #define	REFENTITYNUM_MASK	((1<<REFENTITYNUM_BITS) - 1)
 // the last N-bit number (2^REFENTITYNUM_BITS - 1) is reserved for the special world refentity,
 //  and this is reflected by the value of MAX_REFENTITIES (which therefore is not a power-of-2)
