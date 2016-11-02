ced3cdd7de7cfc393d19fd803044dff6a1cff0a3
eugene <eugene@832414e2-6573-9c40-a513-caa1f8131539>
2016-11-02 18:33:24 +0000

    Better fix for q3wcp17

--- code/renderer/tr_shader.c
+++ code/renderer/tr_shader.c
@@ -2098,17 +2098,18 @@ static void FindLightingStages( void )
 	if ( shader.isSky || ( shader.surfaceFlags & (SURF_NODLIGHT | SURF_SKY) ) || shader.sort > SS_OPAQUE )
 		return;
 
-	shader.lightingStage = 0;
-
-	for ( i = 1; i < shader.numUnfoggedPasses; ++i ) {
+	for ( i = 0; i < shader.numUnfoggedPasses; i++ ) {
 		if ( !stages[i].bundle[0].isLightmap ) {
 			if ( stages[i].bundle[0].tcGen != TCGEN_TEXTURE )
 				continue;
 			if ( (stages[i].stateBits & BLEND_MASK) == (GLS_SRCBLEND_ONE | GLS_DSTBLEND_ONE) )
 				continue;
 			 // fix for q3wcp17' textures/scanctf2/bounce_white and others
-			if ( stages[i].rgbGen == CGEN_IDENTITY && (stages[i].stateBits & BLEND_MASK) == (GLS_SRCBLEND_DST_COLOR | GLS_DSTBLEND_ZERO) )
-				continue;
+			if ( stages[i].rgbGen == CGEN_IDENTITY && (stages[i].stateBits & BLEND_MASK) == (GLS_SRCBLEND_DST_COLOR | GLS_DSTBLEND_ZERO) ) {
+				if ( shader.lightingStage >= 0 ) {
+					continue;
+				}
+			}
 			shader.lightingStage = i;
 		}
 	}
