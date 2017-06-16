ec-/Quake3e

commit caf6b5dc383fa5bb0af64f7408687a2ae77a99fc

Win32: fix AltGg+Enter hotkey on non-US layouts

code/win32/win_wndproc.c
@@ -747,14 +747,12 @@ LRESULT WINAPI MainWndProc( HWND hWnd, UINT uMsg, WPARAM  wParam, LPARAM lParam 
 		break;
 
 	case WM_SYSKEYDOWN:
-		if ( wParam == VK_RETURN )
-		{
+	case WM_KEYDOWN:
+		if ( wParam == VK_RETURN && ( uMsg == WM_SYSKEYDOWN || GetAsyncKeyState( VK_RMENU ) ) ) {
 			Cvar_SetValue( "r_fullscreen", glw_state.cdsFullscreen? 0 : 1 );
 				Cbuf_AddText( "vid_restart\n" );
 			return 0;
 		}
-		// fall through
-	case WM_KEYDOWN:
 		//Com_Printf( "^2k+^7 wParam:%08x lParam:%08x\n", wParam, lParam );
 		Sys_QueEvent( g_wv.sysMsgTime, SE_KEY, MapKey( wParam, lParam ), qtrue, 0, NULL );
 		break;

commit 66dedde09495448760e94231dbb1a3d709334854

Fix stuck AltGr key, regression introduced in https://github.com/ec-/Quake3e/commit/caf6b5dc383fa5bb0af64f7408687a2ae77a99fc

code/win32/win_wndproc.c
@@ -748,7 +748,7 @@ LRESULT WINAPI MainWndProc( HWND hWnd, UINT uMsg, WPARAM  wParam, LPARAM lParam 
 
 	case WM_SYSKEYDOWN:
 	case WM_KEYDOWN:
-		if ( wParam == VK_RETURN && ( uMsg == WM_SYSKEYDOWN || GetAsyncKeyState( VK_RMENU ) ) ) {
+		if ( wParam == VK_RETURN && ( uMsg == WM_SYSKEYDOWN || GetAsyncKeyState( VK_RMENU ) & 0x8000 ) ) {
 			Cvar_SetValue( "r_fullscreen", glw_state.cdsFullscreen? 0 : 1 );
 				Cbuf_AddText( "vid_restart\n" );
 			return 0;