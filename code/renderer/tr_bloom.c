848046f751b5acd4fd8588c36d49d3a866bfbdb8
ec- <tweels@gmail.com>
2017-10-05 23:00:55 +0300

    Increase max. r_bloom2_filter_size to 16 Backport cascaded bloom from renderer_oa

--- code/renderer/tr_bloom.c
+++ code/renderer/tr_bloom.c
@@ -28,6 +28,12 @@ static cvar_t *r_bloom_alpha;
 static cvar_t *r_bloom_darken;
 static cvar_t *r_bloom_intensity;
 static cvar_t *r_bloom_diamond_size;
+static cvar_t *r_bloom_cascade;
+static cvar_t *r_bloom_cascade_blur;
+static cvar_t *r_bloom_cascade_intensity;
+static cvar_t *r_bloom_cascade_alpha;
+static cvar_t *r_bloom_cascade_dry;
+static cvar_t *r_bloom_dry;
 
 /* 
 ============================================================================== 
@@ -73,6 +79,11 @@ static struct {
 		int		width, height;
 		float	readW, readH;
 	} effect;
+	struct {
+		image_t	*texture;
+		int		width, height;
+		float	readW, readH;
+	} effect2;
 	struct {
 		image_t	*texture;
 		int		width, height;
@@ -139,6 +150,10 @@ static void R_Bloom_InitTextures( void )
 	bloom.effect.readW = bloom.work.width / (float)bloom.effect.width;
 	bloom.effect.readH = bloom.work.height / (float)bloom.effect.height;
 
+	bloom.effect2.readW=bloom.effect.readW;
+	bloom.effect2.readH=bloom.effect.readH;
+	bloom.effect2.width=bloom.effect.width;
+	bloom.effect2.height=bloom.effect.height;
 
 	// disable blooms if we can't handle a texture of that size
 	if( bloom.screen.width > glConfig.maxTextureSize ||
@@ -162,7 +177,9 @@ static void R_Bloom_InitTextures( void )
 	data = ri.Hunk_AllocateTempMemory( bloom.effect.width * bloom.effect.height * 4 );
 	Com_Memset( data, 0, bloom.effect.width * bloom.effect.height * 4 );
 	bloom.effect.texture = R_CreateImage( "***bloom effect texture***", data, bloom.effect.width, bloom.effect.height, IMGTYPE_COLORALPHA, IMGFLAG_CLAMPTOEDGE, 0 );
+	bloom.effect2.texture = R_CreateImage( "***bloom effect texture 2***", data, bloom.effect.width, bloom.effect.height, IMGTYPE_COLORALPHA, IMGFLAG_CLAMPTOEDGE, 0 );
 	ri.Hunk_FreeTempMemory( data );
+
 	bloom.started = qtrue;
 }
 
@@ -186,10 +203,107 @@ R_Bloom_DrawEffect
 */
 static void R_Bloom_DrawEffect( void )
 {
+	float alpha = r_bloom_alpha->value;
 	GL_Bind( bloom.effect.texture );
 	GL_State( GLS_DEPTHTEST_DISABLE | GLS_SRCBLEND_ONE | GLS_DSTBLEND_ONE );
-	qglColor4f( r_bloom_alpha->value, r_bloom_alpha->value, r_bloom_alpha->value, 1.0f );
+	if ( r_bloom_cascade->integer ) {
+		alpha = r_bloom_cascade_alpha->value;
+	}
+	qglColor4f( alpha, alpha, alpha, 1.0f );
 	R_Bloom_Quad( glConfig.vidWidth, glConfig.vidHeight, 0, 0, bloom.effect.readW, bloom.effect.readW );
+}
+
+
+/*
+ =================
+ R_Bloom_Cascaded
+ =================
+ Tcpp: sorry for my poor English skill.
+ */
+static void R_Bloom_Cascaded( void )
+{
+	int scale;
+	int oldWorkW, oldWorkH;
+	int newWorkW, newWorkH;
+	float bloomShiftX = r_bloom_cascade_blur->value / (float)bloom.effect.width;
+	float bloomShiftY = r_bloom_cascade_blur->value / (float)bloom.effect.height;
+	float intensity = r_bloom_cascade_intensity->value;
+	float intensity2;
+
+	qglColor4f( 1.0f, 1.0f, 1.0f, 1.0f );
+	//Take the backup texture and downscale it
+	GL_Bind( bloom.screen.texture );
+	GL_State( GLS_DEPTHTEST_DISABLE | GLS_SRCBLEND_ONE | GLS_DSTBLEND_ZERO );
+	R_Bloom_Quad( bloom.work.width, bloom.work.height, 0, 0, bloom.screen.readW, bloom.screen.readH );
+	//Copy downscaled framebuffer into a texture
+	GL_Bind( bloom.effect.texture );
+	qglCopyTexSubImage2D( GL_TEXTURE_2D, 0, 0, 0, 0, 0, bloom.work.width, bloom.work.height );
+
+	/* Copy the result to the effect texture */
+	GL_Bind( bloom.effect2.texture );
+	qglCopyTexSubImage2D( GL_TEXTURE_2D, 0, 0, 0, 0, 0, bloom.work.width, bloom.work.height );
+
+	// do blurs..
+	scale = 32;
+	while ( bloom.work.width < scale )
+		scale >>= 1;
+	while ( bloom.work.height < scale )
+		scale >>= 1;
+
+	// prepare the first level.
+	newWorkW=bloom.work.width/scale;
+	newWorkH=bloom.work.height/scale;
+
+	GL_Bind( bloom.effect2.texture );
+	GL_State( GLS_DEPTHTEST_DISABLE | GLS_SRCBLEND_ONE | GLS_DSTBLEND_ZERO );
+	intensity2 = intensity / (float)scale;
+	qglColor4f( intensity2, intensity2, intensity2, 1.0f );
+	R_Bloom_Quad( newWorkW, newWorkH, 0, 0, bloom.effect2.readW, bloom.effect2.readH );
+
+	// go through levels.
+	while ( scale > 1 ) {
+		float oldScaleInv = 1.0f / (float)scale;
+		scale >>= 1;
+		oldWorkH = newWorkH;
+		oldWorkW = newWorkW;
+		newWorkW = bloom.work.width/scale;
+		newWorkH = bloom.work.height/scale;
+
+		// get effect texture.
+		GL_Bind( bloom.effect.texture );
+		qglCopyTexSubImage2D( GL_TEXTURE_2D, 0, 0, 0, 0, 0, oldWorkW, oldWorkH);
+
+		// maginfy the previous level.
+		if ( r_bloom_cascade_blur->value < 0.01f ) {
+			// don't blur.
+			qglColor4f( 1.0f, 1.0f, 1.0f, 1.0f );
+			GL_State( GLS_DEPTHTEST_DISABLE | GLS_SRCBLEND_ONE | GLS_DSTBLEND_ZERO );
+			R_Bloom_Quad( newWorkW, newWorkH, 0, 0, bloom.effect.readW*oldScaleInv, bloom.effect.readH*oldScaleInv );
+		}
+		else {
+			// blur.
+			qglColor4f( 0.25f, 0.25f, 0.25f, 1.0f );
+			GL_State( GLS_DEPTHTEST_DISABLE | GLS_SRCBLEND_ONE | GLS_DSTBLEND_ZERO );
+			R_Bloom_Quad( newWorkW, newWorkH, -bloomShiftX, -bloomShiftY, bloom.effect.readW*oldScaleInv, bloom.effect.readH*oldScaleInv );
+
+			GL_State( GLS_DEPTHTEST_DISABLE | GLS_SRCBLEND_ONE | GLS_DSTBLEND_ONE );
+			R_Bloom_Quad( newWorkW, newWorkH, bloomShiftX, -bloomShiftY, bloom.effect.readW*oldScaleInv, bloom.effect.readH*oldScaleInv );
+			R_Bloom_Quad( newWorkW, newWorkH, -bloomShiftX, bloomShiftY, bloom.effect.readW*oldScaleInv, bloom.effect.readH*oldScaleInv );
+			R_Bloom_Quad( newWorkW, newWorkH, bloomShiftX, bloomShiftY, bloom.effect.readW*oldScaleInv, bloom.effect.readH*oldScaleInv );
+		}
+
+		// add the input.
+		intensity2 = intensity / (float)scale;
+		qglColor4f( intensity2, intensity2, intensity2, 1.0f );
+
+		GL_State( GLS_DEPTHTEST_DISABLE | GLS_SRCBLEND_ONE | GLS_DSTBLEND_ONE );
+		GL_Bind( bloom.effect2.texture );
+
+		R_Bloom_Quad( newWorkW, newWorkH, 0, 0, bloom.effect2.readW, bloom.effect2.readH );
+	}
+
+	GL_Bind( bloom.effect.texture );
+	qglCopyTexSubImage2D( GL_TEXTURE_2D, 0, 0, 0, 0, 0, bloom.work.width, bloom.work.height );
 }
 
 
@@ -202,7 +316,6 @@ static void R_Bloom_WarsowEffect( void )
 {
 	int		i, j, k;
 	float	intensity, scale, *diamond;
-
 
 	qglColor4f( 1.0f, 1.0f, 1.0f, 1.0f );
 	//Take the backup texture and downscale it
@@ -217,8 +330,7 @@ static void R_Bloom_WarsowEffect( void )
 	if ( r_bloom_darken->integer ) {
 		GL_State( GLS_DEPTHTEST_DISABLE | GLS_SRCBLEND_DST_COLOR | GLS_DSTBLEND_ZERO );
 		for( i = 0; i < r_bloom_darken->integer; i++ ) {
-			R_Bloom_Quad( bloom.work.width, bloom.work.height, 0, 0, 
-				bloom.effect.readW, bloom.effect.readH );
+			R_Bloom_Quad( bloom.work.width, bloom.work.height, 0, 0, bloom.effect.readW, bloom.effect.readH );
 		}
 		qglCopyTexSubImage2D( GL_TEXTURE_2D, 0, 0, 0, 0, 0, bloom.work.width, bloom.work.height );
 	}
@@ -263,7 +375,7 @@ static void R_Bloom_WarsowEffect( void )
 		for( j = 0; j < r_bloom_diamond_size->integer; j++, diamond++ ) {
 			float x, y;
 			intensity =  *diamond * scale;
-			if( intensity < 0.01f )
+			if ( intensity < 0.01f )
 				continue;
 			qglColor4f( intensity, intensity, intensity, 1.0 );
 			x = (i - k) * ( 2 / 640.0f ) * bloom.effect.readW;
@@ -273,7 +385,8 @@ static void R_Bloom_WarsowEffect( void )
 		}
 	}
 	qglCopyTexSubImage2D( GL_TEXTURE_2D, 0, 0, 0, 0, 0, bloom.work.width, bloom.work.height );
-}											
+}
+
 
 /*
 =================
@@ -285,6 +398,8 @@ static void R_Bloom_BackupScreen( void ) {
 	GL_Bind( bloom.screen.texture );
 	qglCopyTexSubImage2D( GL_TEXTURE_2D, 0, 0, 0, 0, 0, glConfig.vidWidth, glConfig.vidHeight );
 }
+
+
 /*
 =================
 R_Bloom_RestoreScreen
@@ -292,13 +407,22 @@ Restore the temporary framebuffer section we used with the backup texture
 =================
 */
 static void R_Bloom_RestoreScreen( void ) {
+	float dry = r_bloom_dry->value;
+	if ( r_bloom_cascade->integer )
+		dry = r_bloom_cascade_dry->value;
 	GL_State( GLS_DEPTHTEST_DISABLE | GLS_SRCBLEND_ONE | GLS_DSTBLEND_ZERO );
 	GL_Bind( bloom.screen.texture );
-	qglColor4f( 1, 1, 1, 1 );
-	R_Bloom_Quad( bloom.work.width, bloom.work.height, 0, 0,
-		bloom.work.width / (float)bloom.screen.width,
-		bloom.work.height / (float)bloom.screen.height );
+	qglColor4f( dry, dry, dry, 1.0f );
+	if ( dry < 0.99f ) {
+		R_Bloom_Quad( bloom.screen.width, bloom.screen.height, 0, 0, 1.f, 1.f );
+	}
+	else {
+		R_Bloom_Quad( bloom.work.width, bloom.work.height, 0, 0,
+			bloom.work.width / (float)bloom.screen.width,
+			bloom.work.height / (float)bloom.screen.height );
+	}
 }
+
 
 extern void	RB_SetGL2D (void);
 
@@ -347,13 +471,16 @@ void R_BloomScreen( void )
 	R_Bloom_BackupScreen();
 
 	// create the bloom texture using one of a few methods
-	R_Bloom_WarsowEffect();
+	if ( r_bloom_cascade->integer )
+		R_Bloom_Cascaded();
+	else
+		R_Bloom_WarsowEffect();
 
 	// restore the screen-backup to the screen
 	R_Bloom_RestoreScreen();
 
 	// Do the final pass using the bloom texture for the final effect
-	R_Bloom_DrawEffect ();
+	R_Bloom_DrawEffect();
 }
 
 
@@ -361,10 +488,17 @@ void R_BloomInit( void )
 {
 	memset( &bloom, 0, sizeof( bloom ));
 
-	r_bloom = ri.Cvar_Get( "r_bloom", "0", CVAR_ARCHIVE_ND );
+	r_bloom = ri.Cvar_Get( "r_bloom", "0", CVAR_ARCHIVE );
 	r_bloom_alpha = ri.Cvar_Get( "r_bloom_alpha", "0.5", CVAR_ARCHIVE_ND );
 	r_bloom_diamond_size = ri.Cvar_Get( "r_bloom_diamond_size", "8", CVAR_ARCHIVE_ND );
 	r_bloom_intensity = ri.Cvar_Get( "r_bloom_intensity", "2.0", CVAR_ARCHIVE_ND );
 	r_bloom_darken = ri.Cvar_Get( "r_bloom_darken", "2", CVAR_ARCHIVE_ND );
 	r_bloom_sample_size = ri.Cvar_Get( "r_bloom_sample_size", "128", CVAR_ARCHIVE_ND | CVAR_LATCH );
+
+	r_bloom_cascade = ri.Cvar_Get( "r_bloom_cascade", "0", CVAR_ARCHIVE );
+	r_bloom_cascade_blur = ri.Cvar_Get( "r_bloom_cascade_blur", ".4", CVAR_ARCHIVE_ND );
+	r_bloom_cascade_intensity= ri.Cvar_Get( "r_bloom_cascade_intensity", "20", CVAR_ARCHIVE_ND );
+	r_bloom_cascade_alpha = ri.Cvar_Get( "r_bloom_cascade_alpha", "0.15", CVAR_ARCHIVE_ND );
+	r_bloom_cascade_dry = ri.Cvar_Get( "r_bloom_cascade_dry", "0.8", CVAR_ARCHIVE_ND );
+	r_bloom_dry = ri.Cvar_Get( "r_bloom_dry", "1", CVAR_ARCHIVE_ND );
 }
