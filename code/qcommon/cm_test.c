290623a99cadf85f350fede2b715facd2807b2f8
ec- <tweels@gmail.com>
2018-02-10 13:17:12 +0200

    Slightly speedup collision detection for win64-msvc builds

--- code/qcommon/cm_test.c
+++ code/qcommon/cm_test.c
@@ -480,6 +480,9 @@ int CM_WriteAreaBits (byte *buffer, int area)
 	return bytes;
 }
 
+
+#define BOUNDS_CLIP_EPSILON 0.25f // assume single precision and slightly increase to compensate potential SIMD precison loss in 64-bit environment
+
 /*
 ====================
 CM_BoundsIntersect
@@ -487,18 +490,19 @@ CM_BoundsIntersect
 */
 qboolean CM_BoundsIntersect( const vec3_t mins, const vec3_t maxs, const vec3_t mins2, const vec3_t maxs2 )
 {
-	if (maxs[0] < mins2[0] - SURFACE_CLIP_EPSILON ||
-		maxs[1] < mins2[1] - SURFACE_CLIP_EPSILON ||
-		maxs[2] < mins2[2] - SURFACE_CLIP_EPSILON ||
-		mins[0] > maxs2[0] + SURFACE_CLIP_EPSILON ||
-		mins[1] > maxs2[1] + SURFACE_CLIP_EPSILON ||
-		mins[2] > maxs2[2] + SURFACE_CLIP_EPSILON)
+	if (maxs[0] < mins2[0] - BOUNDS_CLIP_EPSILON ||
+		maxs[1] < mins2[1] - BOUNDS_CLIP_EPSILON ||
+		maxs[2] < mins2[2] - BOUNDS_CLIP_EPSILON ||
+		mins[0] > maxs2[0] + BOUNDS_CLIP_EPSILON ||
+		mins[1] > maxs2[1] + BOUNDS_CLIP_EPSILON ||
+		mins[2] > maxs2[2] + BOUNDS_CLIP_EPSILON)
 	{
 		return qfalse;
 	}
 
 	return qtrue;
 }
+
 
 /*
 ====================
@@ -507,12 +511,12 @@ CM_BoundsIntersectPoint
 */
 qboolean CM_BoundsIntersectPoint( const vec3_t mins, const vec3_t maxs, const vec3_t point )
 {
-	if (maxs[0] < point[0] - SURFACE_CLIP_EPSILON ||
-		maxs[1] < point[1] - SURFACE_CLIP_EPSILON ||
-		maxs[2] < point[2] - SURFACE_CLIP_EPSILON ||
-		mins[0] > point[0] + SURFACE_CLIP_EPSILON ||
-		mins[1] > point[1] + SURFACE_CLIP_EPSILON ||
-		mins[2] > point[2] + SURFACE_CLIP_EPSILON)
+	if (maxs[0] < point[0] - BOUNDS_CLIP_EPSILON ||
+		maxs[1] < point[1] - BOUNDS_CLIP_EPSILON ||
+		maxs[2] < point[2] - BOUNDS_CLIP_EPSILON ||
+		mins[0] > point[0] + BOUNDS_CLIP_EPSILON ||
+		mins[1] > point[1] + BOUNDS_CLIP_EPSILON ||
+		mins[2] > point[2] + BOUNDS_CLIP_EPSILON)
 	{
 		return qfalse;
 	}
