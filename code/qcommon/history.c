308b1bfe45dcfc28bd134149aa4c67ed34c22666
ec- <tweels@gmail.com>
2018-04-09 14:16:45 +0300

    ioq3: Fix q3history buffer not cleared between mods and OOB-access

--- code/qcommon/history.c
+++ code/qcommon/history.c
@@ -130,9 +130,10 @@ static void Con_LoadHistory( void )
 		return;
 	}
 
-	if( consoleSaveBufferSize <= MAX_CONSOLE_SAVE_BUFFER &&
+	if ( consoleSaveBufferSize < MAX_CONSOLE_SAVE_BUFFER &&
 			FS_Read( consoleSaveBuffer, consoleSaveBufferSize, f ) == consoleSaveBufferSize )
 	{
+		consoleSaveBuffer[ consoleSaveBufferSize ] = '\0';
 		text_p = consoleSaveBuffer;
 
 		for( i = COMMAND_HISTORY - 1; i >= 0; i-- )
