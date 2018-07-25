fa0186cc61ec1131e25fd4f8d15403b871016bdf
ec- <tweels@gmail.com>
2018-07-26 00:07:24 +0300

    Fixed 'floating' console color

--- code/client/cl_console.c
+++ code/client/cl_console.c
@@ -771,6 +771,7 @@ void Con_DrawSolidConsole( float frac ) {
 			re.SetColor( conColorValue );
 			re.DrawStretchPic( 0, 0, wf, yf, 0, 0, 1, 1, cls.whiteShader );
 		} else {
+			re.SetColor( g_color_table[ ColorIndex( COLOR_WHITE ) ] );
 			re.DrawStretchPic( 0, 0, wf, yf, 0, 0, 1, 1, cls.consoleShader );
 		}
 
