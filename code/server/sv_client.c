c4630c26dc8a126c7fac4b945f12d0b99dbeccae
ec- <tweels@gmail.com>
2019-06-04 20:15:06 +0300

    Allow single ipv4 ranges

--- code/server/sv_client.c
+++ code/server/sv_client.c
@@ -292,7 +292,7 @@ static qboolean SV_LoadIP4DB( const char *filename )
 #endif
 		if ( last_ip && last_ip >= ipdb_range[i].from )
 			break;
-		if ( ipdb_range[i].from >= ipdb_range[i].to )
+		if ( ipdb_range[i].from > ipdb_range[i].to )
 			break;
 		if ( ipdb_tld[i].tld[0] < 'A' || ipdb_tld[i].tld[0] > 'Z' || ipdb_tld[i].tld[1] < 'A' || ipdb_tld[i].tld[1] > 'Z' )
 			break;
