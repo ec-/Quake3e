bdd39cdd627b46acbb011087d0529db82a848549
ec- <tweels@gmail.com>
2019-10-29 21:40:12 +0200

    Do the same in console code

--- code/win32/win_syscon.c
+++ code/win32/win_syscon.c
@@ -361,7 +361,7 @@ static LRESULT WINAPI ConWndProc( HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lP
 #ifdef DEDICATED
 			Com_Frame( qfalse );
 #else
-			Com_Frame( CL_NoDelay() );
+			//Com_Frame( CL_NoDelay() );
 #endif
 		}
 		break;
@@ -412,7 +412,7 @@ static LRESULT WINAPI BufferWndProc( HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM
 #ifdef DEDICATED
 			Com_Frame( qfalse );
 #else
-			Com_Frame( CL_NoDelay() );
+			//Com_Frame( CL_NoDelay() );
 #endif
 		}
 		if ( wParam == TEX_TIMER_ID && texTimerID != 0 ) {
