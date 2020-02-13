0ef9a10fa61ccb700db4201d476345dfdc59ac57
Eugene <tweels@gmail.com>
2020-02-14 00:34:38 +0200

    Removed CVAR_CHEAT flag for \r_noportals cvar

--- code/renderervk/tr_init.c
+++ code/renderervk/tr_init.c
@@ -1563,7 +1563,7 @@ static void R_Register( void )
 	r_offsetUnits = ri.Cvar_Get( "r_offsetunits", "-2", CVAR_CHEAT | CVAR_LATCH );
 	r_drawBuffer = ri.Cvar_Get( "r_drawBuffer", "GL_BACK", CVAR_CHEAT );
 	r_lockpvs = ri.Cvar_Get ("r_lockpvs", "0", CVAR_CHEAT);
-	r_noportals = ri.Cvar_Get ("r_noportals", "0", CVAR_CHEAT);
+	r_noportals = ri.Cvar_Get ("r_noportals", "0", 0);
 	r_shadows = ri.Cvar_Get( "cg_shadows", "1", 0 );
 
 	r_marksOnTriangleMeshes = ri.Cvar_Get("r_marksOnTriangleMeshes", "0", CVAR_ARCHIVE_ND );
