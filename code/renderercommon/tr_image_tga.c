5aeb267b46c7583f201f8e6096a8502c52e4da1b
mediumBeast <74370030+mediumBeast@users.noreply.github.com>
2021-01-02 14:20:18 +0100

    print warning about top-down tga image only in developer mode

--- code/renderercommon/tr_image_tga.c
+++ code/renderercommon/tr_image_tga.c
@@ -302,7 +302,7 @@ void R_LoadTGA ( const char *name, byte **pic, int *width, int *height)
 #endif
   // instead we just print a warning
   if (targa_header.attributes & 0x20) {
-    ri.Printf( PRINT_WARNING, "WARNING: '%s' TGA file header declares top-down image, ignoring\n", name);
+    ri.Printf( PRINT_DEVELOPER, "WARNING: '%s' TGA file header declares top-down image, ignoring\n", name);
   }
 
   if (width)
