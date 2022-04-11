3d64cde9d01c57134d4b1f881eed69b15b7f6b48
Eugene <tweels@gmal.com>
2022-04-11 15:22:10 +0300

    Fixed missing video/audio frames during multi-segment avi rendering

--- code/client/client.h
+++ code/client/client.h
@@ -584,11 +584,11 @@ qboolean CL_Netchan_Process( netchan_t *chan, msg_t *msg );
 //
 // cl_avi.c
 //
-qboolean CL_OpenAVIForWriting( const char *filename, qboolean pipe );
+qboolean CL_OpenAVIForWriting( const char *filename, qboolean pipe, qboolean reopen );
 void CL_TakeVideoFrame( void );
 void CL_WriteAVIVideoFrame( const byte *imageBuffer, int size );
 void CL_WriteAVIAudioFrame( const byte *pcmBuffer, int size );
-qboolean CL_CloseAVI( void );
+qboolean CL_CloseAVI( qboolean reopen );
 qboolean CL_VideoRecording( void );
 
 //
