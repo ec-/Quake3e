998f35e51837bcd3002df5de27628382873a8e41
Eugene <tweels@gmail.com>
2023-04-17 00:34:54 +0300

    better fix

--- code/qcommon/q_math.c
+++ code/qcommon/q_math.c
@@ -1124,7 +1124,8 @@ Q_log2f
 */
 float Q_log2f( float f )
 {
-	return logf(( f ) / M_LN2);		// VS2005: added parens to avoid compiler syntax error
+	const float v = logf( f );
+	return v / M_LN2;
 }
 
 
