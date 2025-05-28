ec-/Quake3e

commit e4a88f9a2171f7ef606e9e010aa675f150d35a99

Vulkan: do not print presentation info at swapchain restart

code/renderervk/vk.c
@@ -476,7 +476,7 @@ static void vk_set_object_name( uint64_t obj, const char *objName, VkDebugReport
 }
 
 
-static void vk_create_swapchain( VkPhysicalDevice physical_device, VkDevice device, VkSurfaceKHR surface, VkSurfaceFormatKHR surface_format, VkSwapchainKHR *swapchain ) {
+static void vk_create_swapchain( VkPhysicalDevice physical_device, VkDevice device, VkSurfaceKHR surface, VkSurfaceFormatKHR surface_format, VkSwapchainKHR *swapchain, qboolean verbose ) {
 	VkImageViewCreateInfo view;
 	VkSurfaceCapabilitiesKHR surface_caps;
 	VkExtent2D image_extent;
@@ -518,18 +518,20 @@ static void vk_create_swapchain( VkPhysicalDevice physical_device, VkDevice devi
 	present_modes = (VkPresentModeKHR *) ri.Malloc( present_mode_count * sizeof( VkPresentModeKHR ) );
 	VK_CHECK(qvkGetPhysicalDeviceSurfacePresentModesKHR(physical_device, surface, &present_mode_count, present_modes));
 
-	ri.Printf( PRINT_ALL, "...presentation modes:" );
-	for ( i = 0; i < present_mode_count; i++ ) {
-		ri.Printf( PRINT_ALL, " %s", pmode_to_str( present_modes[i] ) );
-		if ( present_modes[i] == VK_PRESENT_MODE_MAILBOX_KHR )
-			mailbox_supported = qtrue;
-		else if ( present_modes[i] == VK_PRESENT_MODE_IMMEDIATE_KHR )
-			immediate_supported = qtrue;
-		else if ( present_modes[i] ==  VK_PRESENT_MODE_FIFO_RELAXED_KHR )
-			fifo_relaxed_supported = qtrue;
+	if ( verbose ) {
+		ri.Printf( PRINT_ALL, "...presentation modes:" );
+		for ( i = 0; i < present_mode_count; i++ ) {
+			ri.Printf( PRINT_ALL, " %s", pmode_to_str( present_modes[i] ) );
+			if ( present_modes[i] == VK_PRESENT_MODE_MAILBOX_KHR )
+				mailbox_supported = qtrue;
+			else if ( present_modes[i] == VK_PRESENT_MODE_IMMEDIATE_KHR )
+				immediate_supported = qtrue;
+			else if ( present_modes[i] == VK_PRESENT_MODE_FIFO_RELAXED_KHR )
+				fifo_relaxed_supported = qtrue;
 
+		}
+		ri.Printf( PRINT_ALL, "\n" );
 	}
-	ri.Printf( PRINT_ALL, "\n" );
 
 	ri.Free( present_modes );
 
@@ -567,7 +569,9 @@ static void vk_create_swapchain( VkPhysicalDevice physical_device, VkDevice devi
 		image_count = MIN( MIN( image_count, surface_caps.maxImageCount ), MAX_SWAPCHAIN_IMAGES );
 	}
 
-	ri.Printf( PRINT_ALL, "...selected presentation mode: %s, image count: %i\n", pmode_to_str( present_mode ), image_count );
+	if ( verbose ) {
+		ri.Printf( PRINT_ALL, "...selected presentation mode: %s, image count: %i\n", pmode_to_str( present_mode ), image_count );
+	}
 
 	// create swap chain
 	desc.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
@@ -3866,7 +3870,7 @@ static void vk_restart_swapchain( const char *funcname )
 	setup_surface_formats( vk.physical_device );
 
 	vk_create_sync_primitives();
-	vk_create_swapchain( vk.physical_device, vk.device, vk_surface, vk.present_format, &vk.swapchain );
+	vk_create_swapchain( vk.physical_device, vk.device, vk_surface, vk.present_format, &vk.swapchain, qfalse );
 	vk_create_attachments();
 	vk_create_render_passes();
 	vk_create_framebuffers();
@@ -4255,7 +4259,7 @@ void vk_initialize( void )
 	// swapchain
 	vk.initSwapchainLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
 	//vk.initSwapchainLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-	vk_create_swapchain( vk.physical_device, vk.device, vk_surface, vk.present_format, &vk.swapchain );
+	vk_create_swapchain( vk.physical_device, vk.device, vk_surface, vk.present_format, &vk.swapchain, qtrue );
 
 	// color/depth attachments
 	vk_create_attachments();

commit 829e8794111199c58f257b96234ff441df5de393

Vulkan: fix swapchain re-creation regression, introduced in e4a88f9a2171f7ef606e9e010aa675f150d35a99

code/renderervk/vk.c
@@ -520,16 +520,19 @@ static void vk_create_swapchain( VkPhysicalDevice physical_device, VkDevice devi
 
 	if ( verbose ) {
 		ri.Printf( PRINT_ALL, "...presentation modes:" );
-		for ( i = 0; i < present_mode_count; i++ ) {
+	}
+	for ( i = 0; i < present_mode_count; i++ ) {
+		if ( verbose ) {
 			ri.Printf( PRINT_ALL, " %s", pmode_to_str( present_modes[i] ) );
-			if ( present_modes[i] == VK_PRESENT_MODE_MAILBOX_KHR )
-				mailbox_supported = qtrue;
-			else if ( present_modes[i] == VK_PRESENT_MODE_IMMEDIATE_KHR )
-				immediate_supported = qtrue;
-			else if ( present_modes[i] == VK_PRESENT_MODE_FIFO_RELAXED_KHR )
-				fifo_relaxed_supported = qtrue;
-
 		}
+		if ( present_modes[i] == VK_PRESENT_MODE_MAILBOX_KHR )
+			mailbox_supported = qtrue;
+		else if ( present_modes[i] == VK_PRESENT_MODE_IMMEDIATE_KHR )
+			immediate_supported = qtrue;
+		else if ( present_modes[i] == VK_PRESENT_MODE_FIFO_RELAXED_KHR )
+			fifo_relaxed_supported = qtrue;
+	}
+	if ( verbose ) {
 		ri.Printf( PRINT_ALL, "\n" );
 	}
 
@@ -3853,10 +3856,15 @@ static void vk_destroy_attachments( void );
 static void vk_destroy_render_passes( void );
 static void vk_destroy_pipelines( qboolean resetCount );
 
-static void vk_restart_swapchain( const char *funcname )
+static void vk_restart_swapchain( const char *funcname, VkResult res )
 {
 	uint32_t i;
-	ri.Printf( PRINT_WARNING, "%s(): restarting swapchain...\n", funcname );
+
+#ifdef _DEBUG
+	ri.Printf( PRINT_WARNING, "%s(%s): restarting swapchain...\n", funcname, vk_result_string( res ) );
+#else
+	ri.Printf(PRINT_WARNING, "%s(): restarting swapchain...\n", funcname );
+#endif
 
 	vk_wait_idle();
 
@@ -7289,7 +7297,7 @@ _retry:
 			if ( res == VK_ERROR_OUT_OF_DATE_KHR && retry == qfalse ) {
 				// swapchain re-creation needed
 				retry = qtrue;
-				vk_restart_swapchain( __func__ );
+				vk_restart_swapchain( __func__, res );
 				goto _retry;
 			} else {
 				ri.Error( ERR_FATAL, "vkAcquireNextImageKHR returned %s", vk_result_string( res ) );
@@ -7541,8 +7549,8 @@ void vk_present_frame( void )
 		case VK_SUBOPTIMAL_KHR:
 		case VK_ERROR_OUT_OF_DATE_KHR:
 			// swapchain re-creation needed
-			vk_restart_swapchain( __func__ );
-			break;
+			vk_restart_swapchain( __func__, res );
+			return;
 		case VK_ERROR_DEVICE_LOST:
 			// we can ignore that
 			ri.Printf( PRINT_DEVELOPER, "vkQueuePresentKHR: device lost\n" );