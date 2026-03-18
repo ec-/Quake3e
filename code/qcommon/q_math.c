6ca45dc7dc6ecc40c87835e0ef51b0dc012f3753
skullernet <255839533+skullernet@users.noreply.github.com>
2026-03-18 14:51:03 +0300

    Simplify RotatePointAroundVector().
    More compact code with less math operations. Adapted from Mesa 3D.

--- code/qcommon/q_math.c
+++ code/qcommon/q_math.c
@@ -342,66 +342,55 @@ qboolean PlaneFromPoints( vec4_t plane, const vec3_t a, const vec3_t b, const ve
 }
 
 /*
+==================
+SetupRotationMatrix
+
+Setup rotation matrix given the normalized direction vector and angle to rotate
+around this vector. Adapted from Mesa 3D.
+==================
+*/
+void SetupRotationMatrix( vec3_t matrix[3], const vec3_t dir, float degrees ) {
+	vec_t	angle, s, c, one_c, xx, yy, zz, xy, yz, zx, xs, ys, zs;
+
+	angle = DEG2RAD(degrees);
+	s = sin(angle);
+	c = cos(angle);
+	one_c = 1.0F - c;
+
+	xx = dir[0] * dir[0];
+	yy = dir[1] * dir[1];
+	zz = dir[2] * dir[2];
+	xy = dir[0] * dir[1];
+	yz = dir[1] * dir[2];
+	zx = dir[2] * dir[0];
+	xs = dir[0] * s;
+	ys = dir[1] * s;
+	zs = dir[2] * s;
+
+	matrix[0][0] = (one_c * xx) + c;
+	matrix[0][1] = (one_c * xy) - zs;
+	matrix[0][2] = (one_c * zx) + ys;
+
+	matrix[1][0] = (one_c * xy) + zs;
+	matrix[1][1] = (one_c * yy) + c;
+	matrix[1][2] = (one_c * yz) - xs;
+
+	matrix[2][0] = (one_c * zx) - ys;
+	matrix[2][1] = (one_c * yz) + xs;
+	matrix[2][2] = (one_c * zz) + c;
+}
+
+/*
 ===============
 RotatePointAroundVector
-
-This is not implemented very well...
 ===============
 */
 void RotatePointAroundVector( vec3_t dst, const vec3_t dir, const vec3_t point,
 							 float degrees ) {
-	float	m[3][3];
-	float	im[3][3];
-	float	zrot[3][3];
-	float	tmpmat[3][3];
-	float	rot[3][3];
-	int	i;
-	vec3_t vr, vup, vf;
-	float	rad;
+	vec3_t matrix[3];
 
-	vf[0] = dir[0];
-	vf[1] = dir[1];
-	vf[2] = dir[2];
-
-	PerpendicularVector( vr, dir );
-	CrossProduct( vr, vf, vup );
-
-	m[0][0] = vr[0];
-	m[1][0] = vr[1];
-	m[2][0] = vr[2];
-
-	m[0][1] = vup[0];
-	m[1][1] = vup[1];
-	m[2][1] = vup[2];
-
-	m[0][2] = vf[0];
-	m[1][2] = vf[1];
-	m[2][2] = vf[2];
-
-	memcpy( im, m, sizeof( im ) );
-
-	im[0][1] = m[1][0];
-	im[0][2] = m[2][0];
-	im[1][0] = m[0][1];
-	im[1][2] = m[2][1];
-	im[2][0] = m[0][2];
-	im[2][1] = m[1][2];
-
-	memset( zrot, 0, sizeof( zrot ) );
-	zrot[0][0] = zrot[1][1] = zrot[2][2] = 1.0F;
-
-	rad = DEG2RAD( degrees );
-	zrot[0][0] = cos( rad );
-	zrot[0][1] = sin( rad );
-	zrot[1][0] = -sin( rad );
-	zrot[1][1] = cos( rad );
-
-	MatrixMultiply( m, zrot, tmpmat );
-	MatrixMultiply( tmpmat, im, rot );
-
-	for ( i = 0; i < 3; i++ ) {
-		dst[i] = rot[i][0] * point[0] + rot[i][1] * point[1] + rot[i][2] * point[2];
-	}
+	SetupRotationMatrix(matrix, dir, degrees);
+	VectorRotate(point, matrix, dst);
 }
 
 /*
