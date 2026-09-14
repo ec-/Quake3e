56a3a58dc05f4c51593c5dbf50f83484241367bc
Matt Setaro <matthewsetaro@proton.me>
2026-09-14 17:42:30 -0400

    botlib: initialize the complete movement result

--- code/botlib/be_ai_move.c
+++ code/botlib/be_ai_move.c
@@ -3013,12 +3013,7 @@ void BotMoveToGoal(bot_moveresult_t *result, int movestate, bot_goal_t *goal, in
 	//bsp_trace_t trace;
 	//static int debugline;
 
-	result->failure = qfalse;
-	result->type = 0;
-	result->blocked = qfalse;
-	result->blockentity = 0;
-	result->traveltype = 0;
-	result->flags = 0;
+	Com_Memset(result, 0, sizeof(*result));
 
 	//
 	ms = BotMoveStateFromHandle(movestate);
